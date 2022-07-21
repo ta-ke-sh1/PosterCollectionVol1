871b3f41247dba960fa648828f628ebcfc6a2e47
takesh1 <trunght.yrc@gmail.com>
2022-07-21 17:59:06 +0700

    title adding + daily poster update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import { BrowserRouter as Router, useRoutes } from "react-router-dom";
 import Homepage from "./components/homepage";
 import Gallery from "./components/gallery";
-import Locomotive from "./components/locomotive";
-import "./styles/root.scss";
 
 const App = () => {
     const routes = useRoutes([
         { path: "/", element: <Homepage /> },
         { path: "/gallery", element: <Gallery /> },
-        { path: "/locomotive", element: <Locomotive /> },
     ]);
     return routes;
 };
--- src/components/gallery.js
+++ src/components/gallery.js
@@ -3,18 +3,16 @@ import "../styles/base.scss";
 import "../styles/gallery.scss";
 import { gsap } from "gsap";
 import { Flip } from "gsap/Flip";
-import { Observer } from "gsap/Observer";
-import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
 gsap.registerPlugin(Flip);
-gsap.registerPlugin(Observer);
-gsap.registerPlugin(ScrollToPlugin);
 
-const POSTER_COUNT = 25;
+const POSTER_COUNT = 26;
 let winsize = { width: window.innerWidth, height: window.innerHeight };
 window.addEventListener("resize", () => {
     winsize = { width: window.innerWidth, height: window.innerHeight };
 });
+
+
 
 export default function Gallery() {
     const [isAnimating, updateAnimation] = useState(false);
@@ -27,31 +25,39 @@ export default function Gallery() {
     const [items, setItems] = useState(null);
     const [slides, setSlides] = useState(null);
     const [wrap, setWrap] = useState(null);
+    const [content, setContent] = useState(null);
 
     useEffect(() => {
         setEl(document.querySelector(".stack"));
         setItems(document.querySelectorAll(".stack-item"));
         setSlides(document.querySelector(".slides"));
         setWrap(document.querySelector(".stack-wrap"));
+        setContent(document.querySelector('.content'));
+        
     }, []);
 
-    const openSlideshow = (name, index) => {
+    const openSlideshow = (index) => {
         if (isAnimating || isOpen) {
             return;
         }
 
         updateAnimation(true);
         updateIndex(index);
-        console.log(index);
 
         const scrollY = window.scrollY;
         document.body.classList.add("oh");
+        content.classList.add('content--open');
+
+        const contentText = new ContentItem(document.getElementById(`content-${index}`));
+        contentText.DOM.el.classList.add('content-item-current');
 
         const state = Flip.getState(items, { props: "opacity" });
         slides.appendChild(el);
 
         const item = document.getElementById(`poster-day-${index}`);
         const itemCenter = item.offsetTop + item.offsetHeight / 2;
+
+        //
 
         document.documentElement.scrollTop = document.body.scrollTop = 0;
         gsap.set(el, {
@@ -60,25 +66,35 @@ export default function Gallery() {
         document.documentElement.scrollTop = document.body.scrollTop = 0;
 
         Flip.from(state, {
-            duration: 1,
+            duration: 1.5,
             ease: "expo",
             onComplete: () => {
                 updateAnimation(false);
                 updateOpen(true);
             },
             onStart: () =>
-                (document.documentElement.scrollTop = document.body.scrollTop =
-                    scrollY),
+            (document.documentElement.scrollTop = document.body.scrollTop =
+                scrollY),
             absoluteOnLeave: true,
-        });
+        })
+            .to(contentText.DOM.texts, {
+                duration: 1.5,
+                ease: 'expo',
+                startAt: { yPercent: 101 },
+                yPercent: 0
+            }, 0);
     };
 
     const closeSlideshow = () => {
-        if (isAnimating) {
+        if (isAnimating || !isOpen) {
             return;
         }
 
+        updateAnimation(true);
+        const contentText = new ContentItem(document.getElementById(`content-${index}`));
+
         document.body.classList.remove("oh");
+
         const state = Flip.getState(items, { props: "opacity" });
         wrap.appendChild(el);
 
@@ -87,15 +103,22 @@ export default function Gallery() {
         });
 
         Flip.from(state, {
-            duration: 1,
+            duration: 1.5,
             ease: "expo",
             onComplete: () => {
+                content.classList.remove('content--open');
+                contentText.DOM.el.classList.remove('content-item-current');
                 updateAnimation(false);
                 updateOpen(false);
                 updateIndex(-1);
             },
             absoluteOnLeave: true,
-        });
+        })
+            .to(contentText.DOM.texts, {
+                duration: 1.5,
+                ease: 'expo',
+                yPercent: -101
+            }, 0);
     };
 
     const navigation = (direction) => {
@@ -112,7 +135,7 @@ export default function Gallery() {
         gsap.set(next, { opacity: status < posterContent.length ? 1 : 0 });
 
         gsap.timeline().to(el, {
-            duration: 1,
+            duration: 1.5,
             ease: "expo",
             y:
                 direction === "next"
@@ -124,14 +147,14 @@ export default function Gallery() {
         });
     };
 
-    const onMouseDown = (name, index) => {
+    const onMouseDown = (index) => {
         // Interfering
         if (isAnimating) {
             return;
         } else if (isOpen) {
             closeSlideshow();
         }
-        openSlideshow(name, index);
+        openSlideshow(index);
     };
 
     let postersNumber = Array.from(
@@ -139,24 +162,34 @@ export default function Gallery() {
         (_, index) => index + 1
     );
     return (
-        <>
-            {/* <div className="content">
-                <ContentCollection />
-            </div> */}
-            <div className="nav-wrap">
-                <div
-                    className="btn"
-                    id="prev"
-                    onMouseDown={() => navigation("prev")}
-                >
-                    Up
-                </div>
-                <div
-                    className="btn"
-                    id="next"
-                    onMouseDown={() => navigation("next")}
-                >
-                    Down
+        <main>
+            <div className="content">
+                {postersNumber.map((number) => (
+                    <Content key={"content" + number} index={number} />
+                ))}
+                <div className="nav-wrap">
+                    <button
+                        className="content__nav btn unbutton"
+                        id="prev"
+                        onClick={() => navigation("prev")}
+                    >
+                        <svg width="100" height="267" viewBox="0 0 100 267">
+                            <path d="M49.894 2.766v262.979"
+                                strokeLinecap="square" />
+                            <path fill="none"
+                                d="M99.75 76.596C73.902 76.596 52.62 43.07 49.895 0 47.168 43.07 25.886 76.596.036 76.596" />
+                        </svg>
+                    </button>
+                    <div
+                        className="content__nav btn unbutton"
+                        id="next"
+                        onClick={() => navigation("next")}
+                    >
+                        <svg width="100" height="267" viewBox="0 0 100 267">
+                            <path d="M49.894 2.766v262.979" strokeLinecap="square" />
+                            <path fill="none" d="M99.75 76.596C73.902 76.596 52.62 43.07 49.895 0 47.168 43.07 25.886 76.596.036 76.596" />
+                        </svg>
+                    </div>
                 </div>
             </div>
             <div className="slides"></div>
@@ -166,24 +199,23 @@ export default function Gallery() {
                     {postersNumber.map((number) => (
                         <Poster
                             key={"poster" + number}
-                            backgroundImage={`url(${
-                                process.env.PUBLIC_URL +
+                            backgroundImage={`url(${process.env.PUBLIC_URL +
                                 "/posters/day" +
                                 number +
                                 ".jpg"
-                            })`}
+                                })`}
                             name={"day-" + number}
                             index={number}
                             content={"temporary"}
-                            onMouseDown={(name, index) =>
-                                onMouseDown(name, index)
+                            onMouseDown={(index) =>
+                                onMouseDown(index)
                             }
                         />
                     ))}
                     <div className="stack-item stack-item--empty"></div>
                 </div>
             </div>
-        </>
+        </main>
     );
 }
 
@@ -196,41 +228,64 @@ const Poster = (props) => {
             name={props.name}
             index={props.index}
             content={props.content}
-            onMouseDown={() => props.onMouseDown(props.name, props.index)}
+            onMouseDown={() => props.onMouseDown(props.index)}
         ></div>
     );
 };
 
-// eslint-disable-next-line
-class ContentCollection extends React.Component {
-    render() {
-        let postersNumber = Array.from(
-            { length: POSTER_COUNT },
-            (_, index) => index + 1
-        );
-        return (
-            <>
-                {postersNumber.map((element) => (
-                    <Content key={"content" + element} index={element} />
-                ))}
-            </>
-        );
-    }
-}
-
 const Content = (props) => {
-    let name = posterContent[props.index - 1].name;
+    let name = posterContent[props.index - 1].name.split(' ');
     let info = posterContent[props.index - 1].info;
     return (
         <div id={"content-" + props.index} className="content-item">
-            <h2 className="content-item-title">{name}</h2>
+            <h2 className="content-item-title">
+                {name.map((word) =>
+                    <span className="oh" key={word}>
+                        <span className="oh__inner">
+                            {word}
+                        </span>
+                    </span>
+                )}
+            </h2>
             <div className="content-item-info">
-                <p className="oh">Day {props.index}</p>
-                <p className="oh">{info}</p>
+                <p className="oh" >
+                    <strong className="oh__inner">
+                        Day {props.index}
+                    </strong>
+                </p>
+                <p className="oh">
+                    <span className="oh__inner">
+                        {info}
+                    </span>
+                </p>
             </div>
         </div>
     );
 };
+
+class ContentItem {
+    DOM = {
+        // main element (.content__item)
+        el: null,
+        // title (.content__item-title)
+        title: null,
+        // description (.content__item-description)
+        description: null,
+        // all .oh__inner text spans that will animate in/out
+        texts: null
+    }
+
+    /**
+     * Constructor.
+     * @param {NodeList} DOM_el - main element (.content-item)
+     */
+    constructor (DOM_el) {
+        this.DOM.el = DOM_el;
+        this.DOM.title = this.DOM.el.querySelector('.content-item-title');
+        this.DOM.description = this.DOM.el.querySelector('.content-item-info');
+        this.DOM.texts = [...this.DOM.el.querySelectorAll('.oh > .oh__inner')];
+    }
+}
 
 const posterContent = [
     {
@@ -306,7 +361,7 @@ const posterContent = [
         info: "",
     },
     {
-        name: "Inertwined",
+        name: "Intertwined",
         info: "",
     },
     {
@@ -314,7 +369,7 @@ const posterContent = [
         info: "",
     },
     {
-        name: "My Daily Process",
+        name: "Daily Process",
         info: "",
     },
     {
@@ -327,6 +382,14 @@ const posterContent = [
     },
     {
         name: "Diagnosis",
+        info: "",
+    },
+    {
+        name: "Where do we belong?",
+        info: "",
+    },
+    {
+        name: "Gradient",
         info: "",
     },
 ];
